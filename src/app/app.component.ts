4f9ef897458744456e6179936ee79c6be92b7f71
Dheeraj <dheeraj241099@gmail.com>
2022-07-16 15:57:58 +0530

    angularcalc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  toshow='0'
+  currvalue=''
+
+
+  writetoinput(value:string)
+  {   if(this.currvalue=='0'){
+      this.currvalue=value
+    }
+    if(this.currvalue!='0')
+    {this.currvalue=this.currvalue+value
+    }
+    this.toshow = this.currvalue
+  }
+
+equals(){
+  this.toshow=eval(this.currvalue)
+  this.currvalue=this.toshow
+}
+
+clear(){
+  this.currvalue='';
+  this.toshow='0'
+}
+back(){
+  this.currvalue=this.currvalue.slice(0,-1)
+  if(this.currvalue==''){this.currvalue='0'}
+  this.toshow=this.currvalue
+}
+
+
+
+
 }
